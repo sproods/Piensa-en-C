e4639190ad4fe3daa4e8c6adc0c5f399e1339b2e
sproods <106177602+sproods@users.noreply.github.com>
2022-08-10 18:30:04 -0500

    Resolución final del problema 2.

--- Resolución_ProblemasSuplementarios/problema2.c
+++ Resolución_ProblemasSuplementarios/problema2.c
@@ -5,8 +5,8 @@
 void Lectura(float [][12]);
 void Imprime(float [][12]);
 void ventasMes(float [][12]);
-void VentasJulio(float [][12]);
-void departamento3(float [][12]);
+void ventasJulio(float [][12], int);
+void departamento(float [][12], int);
 
 void main(void)
 {
@@ -14,6 +14,9 @@ void main(void)
 
 	Lectura(VEN);
 	Imprime(VEN);
+	ventasMes(VEN);
+	ventasJulio(VEN, 7);
+	departamento(VEN, 3);
 }
 
 void Lectura(float A[][12])
@@ -38,4 +41,57 @@ void Imprime(float A[][12])
 			printf(" %.2f |", A[I][J]);
 		printf("\n");
 	}
+}
+
+void ventasMes(float A[][12])
+{
+	int I, J;
+	float SUM, SUMT = 0.0;
+
+	printf("\nVentas mensuales y monto total:\n\n");
+	for(J = 0; J < 12; J++)
+	{
+		SUM = 0.0;
+		for(I = 0; I < 5; I++)
+			SUM += A[I][J];
+		printf("MES %d: %.2f\n", J + 1, SUM);
+		SUMT += SUM;
+	}
+	printf("\nMonto total del año: %.2f\n", SUMT);
+}
+
+void ventasJulio(float A[][12], int M)
+{
+	int I, J, MAY = 0;
+	float VENMA = A[0][M - 1];
+	for(I = 1; I < 5; I++)
+	{
+		if(A[I][M - 1] > VENMA)
+		{
+			VENMA = A[I][M - 1];
+			MAY = I;
+		}
+	}
+	printf("\nEn el mes de julio, el departamento que obtuvo mayor ventas fue: %d, con ventas de: %.2f\n", MAY + 1, VENMA);
+}
+
+void departamento(float A[][12], int D)
+{
+	int I, J, MESMEN = 0, MESMAY = 0;
+	float VENMEN = A[D - 1][0], VENMAY = A[D - 1][0];
+	for(J = 1; J < 12; J++)
+	{
+		if(A[D - 1][J] < VENMEN)
+		{
+			VENMEN = A[D - 1][J];
+			MESMEN = J;
+		}
+		if(A[D - 1][J] > VENMAY)
+		{
+			VENMAY = A[D - 1][J];
+			MESMAY = J;
+		}
+	}
+	printf("\nEl mes en el que el departamento 3 tuvo mayores ventas fue el: %d, con %.2f en ventas", MESMAY + 1, VENMAY);
+	printf("\nEl mes en el que el departamento 3 tuvo menores ventas fue el: %d, con %.2f en ventas\n", MESMEN + 1, VENMEN);
 }
