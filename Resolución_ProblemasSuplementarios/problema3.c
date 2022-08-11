51a0bd4cc230894e4cf89c86e17b39038153322c
sproods <106177602+sproods@users.noreply.github.com>
2022-08-10 22:30:50 -0500

    Avance de la resulución al problema 3.

---
+++ Resolución_ProblemasSuplementarios/problema3.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+/* El programa realiza el intercambio de los elementos de la primera fila con la última, la segunda con la penúltima y así sucesivamente. */
+
+const int MAX = 50;
+
+void Lectura(float [][MAX], int, int);
+void Intercambia(float [][MAX], int, int);
+void Imprime(float [][MAX], int, int);
+
+void main(void)
+{
+	int M, N;
+	float MAT[MAX][MAX];
+
+	do
+	{
+		printf("Digite el número de filas de la matriz: ");
+		scanf("%d", &M);
+	}
+	while(M < 1 || M > 50);
+
+	do
+	{
+		printf("\nDigite el número de columnas de la matriz: ");
+		scanf("%d", &N);
+	}
+	while(N < 1 || N > 50);
+
+	Lectura(MAT, M, N);
+	Imprime(MAT, M, N);
+	Intercambia(MAT, M, N);
+	Imprime(MAT, M, N);
+}
+
+void Lectura(float A[][MAX], int F, int C)
+{
+	int I, J;
+	for(I = 0; I < F; I++)
+		for(J = 0; J < C; J++)
+		{
+			printf("\nDigite el elemento %d %d: ", I + 1, J + 1);
+			scanf("%f", &A[I][J]);
+		}
+}
+
+void Intercambia(float A[][MAX], int F, int C)
+{
+	int I, J;
+	float AUX;
+	for(I = 0; I < F; I++)
+	{
+		for(J = 0; J < (C / 2); J++)
+		{
+			AUX = A[I][J];
+			A[I][J] = A[I][C - 1 - J];
+			A[I][C - 1 - J] = AUX;
+		}
+	}
+}
+
+void Imprime(float A[][MAX], int F, int C)
+{
+	int I, J;
+	printf("\n");
+	for(I = 0; I < F; I++)
+	{
+		printf("|");
+		for(J = 0; J < C; J++)
+			printf(" %.2f |", A[I][J]);
+		printf("\n");
+	}
+}
