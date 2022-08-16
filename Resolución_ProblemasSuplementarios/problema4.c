4ca52a28ad9849d247ed2912d816a804efb13a72
sproods <106177602+sproods@users.noreply.github.com>
2022-08-15 22:21:08 -0500

    Resolución del problema 4 (para cualquier tipo de cuadrado mágico).

--- Resolución_ProblemasSuplementarios/problema4.c
+++ Resolución_ProblemasSuplementarios/problema4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-const int MAX = 100;
+#define MAX 100
 
 void Lectura(int[][MAX], int);        /* Prototipos de funciones. */
 void Imprime(int[][MAX], int);
@@ -20,6 +20,11 @@ void main(void)
     Lectura(CUA, TAM);
     Imprime(CUA, TAM);
     RES = Prueba(CUA, TAM);
+
+    if(RES == 1)
+        printf("\n¡Es un cuadrado mágico!\n");
+    else
+        printf("\n¡No es un cuadrado mágico!\n");
 }
 
 void Lectura(int A[][MAX], int T)       /* La funión Lectura lee los componentes del arreglo. */
@@ -52,7 +57,68 @@ void Imprime(int A[][MAX], int T)
 
 int Prueba(int A[][MAX], int T)
 {
-    int I, J, SUM;
+    int I, J, E = 1, SUM, SUM1, SUM2, SUM3, SUM4;
     
-    
+    SUM = 0;
+    for(J = 0; J < T; J++)          /* Tomando la suma inicial de la primera fila. */
+    {
+        SUM += A[0][J];
+    }
+
+    for(I = 1; I < T; I++)          /* Comparando las sumatorias de las filas. */
+    {
+        SUM1 = 0;
+        for(J = 0; J < T; J++)
+            SUM1 += A[I][J];
+        
+        if(SUM1 == SUM)
+            ;
+        else
+            E = 0;
+    }
+
+    if(E == 1)                      /* Condición para continuar con las comparaciones. */
+    {
+        for(J = 0; J < T; J++)      /* Comparando las sumatorias de las columnas. */
+        {
+            SUM2 = 0;
+            for(I = 0; I < T; I++)
+                SUM2 += A[I][J];
+            
+            if(SUM2 == SUM)
+                ;
+            else
+                E = 0;
+        }
+
+        if(E == 1)                  /* Condición para continuar con las comparaciones. */
+        {
+            SUM3 = 0;
+            for(I = 0; I < T; I++)  /* Comparando las sumatorias de la diagonal principal. */
+                SUM3 += A[I][I];
+
+            if(SUM3 == SUM)
+                ;
+            else
+                E = 0;
+            
+            if(E == 1)
+            {
+                SUM4 = 0;
+                for(I = 0; I < T; I++)
+                    SUM4 += A[I][T - 1 - I];
+                if(SUM4 == SUM)
+                    return E;
+                else
+                    return E;
+            }
+            else
+                return E;
+        }
+        else
+            return E;
+        
+    }
+    else
+        return E;
 }
