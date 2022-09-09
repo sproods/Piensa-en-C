155e6676af76565d46922e51c8c8b8eb7e959605
sproods <106177602+sproods@users.noreply.github.com>
2022-09-09 11:53:41 -0500

    Actualización del estudio de la librería <string.h>

--- cadenas_de_caracteres/Problemas Suplementarios/warmup8.c
+++ cadenas_de_caracteres/Problemas Suplementarios/warmup8.c
@@ -5,7 +5,7 @@
 
 void main(void)
 {
-    char *texto = "La Luna, la Tierra y el Sol", cadena[50];
+    char *texto = "La Luna, la Tierra y el Sol", cadena[50], fraction[50];
     
     printf("Primera cadena de texto: %s\n\n", texto);
 
@@ -13,5 +13,13 @@ void main(void)
     printf("La cadena copia del primer texto es: %s\n\n", cadena);
 
     strcpy(cadena, "Linus Torvals");
-    printf("La siguiente copia es: %s", cadena);
+    printf("La siguiente copia es: %s\n\n", cadena);
+
+    strncpy(fraction, texto, 7);
+    fraction[7] = '\0';
+    puts(fraction);
+
+    strncpy(cadena, texto, 18);
+    cadena[18] = '\0';
+    puts(cadena);
 }
