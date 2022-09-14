8440f002809783ea821956283f856d7487a1015a
sproods <106177602+sproods@users.noreply.github.com>
2022-09-13 23:55:14 -0500

    Contador de subcaracteres en un cadena de caracteres más grande

---
+++ cadenas_de_caracteres/Problemas Suplementarios/repaso/repaso7.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+// El programa verifica cuantas veces una cadena de caracteres contine a otra cadena de caracteres más pequeña.
+
+void main(void)
+{
+    char string[50], sub[50], *cad = "";
+    int i = 0;
+
+    printf("Digite una cadena de caracteres (que será el superconjunto): \n");
+    gets(string);
+    fflush(stdin);
+    printf("Digite la segunda cadena de caracteres (que será el subconjunto): \n");
+    gets(sub);
+    fflush(stdin);
+
+    cad = strstr(string, sub);
+    puts(cad);
+
+    while (cad != NULL)
+    {
+        i++;
+        cad = strstr(cad + 1, sub);
+    }
+
+    printf("\nEl número de veces que la cadena \"%s\" se repite en la cadena \"%s\" es %d\n",sub, string, i);
+}
