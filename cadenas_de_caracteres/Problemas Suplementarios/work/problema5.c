b26d4e12de113944f9ec39ea95a46ea99c34c6df
sproods <106177602+sproods@users.noreply.github.com>
2022-09-19 22:39:30 -0500

    Modificación de las primeras letras en cada palabra de una frase en una cadena de caracteres.

---
+++ cadenas_de_caracteres/Problemas Suplementarios/work/problema5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+// Mayusculización de primeros caractres de cada palabra de una cadena de caracteres.
+
+void main(void)
+{
+    char texto[50];
+    int i = 0;
+
+    printf("Digite una cadena de caracteres: ");
+    gets(texto);
+
+    while (texto[i] != '\0')
+    {
+        if (texto[i] == ' ')
+        {
+            texto[i + 1] = toupper(texto[i + 1]);
+        }
+
+        i++;
+    }
+
+    puts(texto);
+}
