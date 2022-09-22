92808bf6f737c974f5935b80b46e1cb6dedf20d3
sproods <106177602+sproods@users.noreply.github.com>
2022-09-21 23:31:26 -0500

    Inicio en el desarrollo de la solución del problema número ocho.

---
+++ cadenas_de_caracteres/Problemas Suplementarios/work/problema8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+// Combinación de palabras entre dos cadenas de caracteres.
+
+void main(void)
+{
+    char texto1[50], texto2[50];
+
+    printf("Digite la primera cadena de caracteres: ");
+    gets(texto1);
+    fflush(stdin);
+
+    printf("Digite la segunda cadena de caracteres: ");
+    gets(texto2);
+    fflush(stdin);
+}
