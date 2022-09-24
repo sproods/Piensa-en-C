bf64e659b030be4608cf175ff14440f06023cad6
sproods <106177602+sproods@users.noreply.github.com>
2022-09-23 21:46:00 -0500

    Combinación de palabras entres don cadenas de caracteres- Resolución inconclusa.

--- cadenas_de_caracteres/Problemas Suplementarios/work/problema8.c
+++ cadenas_de_caracteres/Problemas Suplementarios/work/problema8.c
@@ -6,6 +6,7 @@
 void main(void)
 {
     char texto1[50], texto2[50];
+    int i = 0, j = 0;
 
     printf("Digite la primera cadena de caracteres: ");
     gets(texto1);
@@ -14,4 +15,32 @@ void main(void)
     printf("Digite la segunda cadena de caracteres: ");
     gets(texto2);
     fflush(stdin);
+
+    printf("\n");
+
+    while (texto1[i] != '\0' || texto2[j] != '\0')
+    {
+        while ((texto1[i] != ' ') && (texto1[i] != '\0'))
+        {
+            putchar(texto1[i]);
+            i++;
+        }
+        fflush(stdin);
+
+        printf(" ");
+
+        while ((texto2[j] != ' ') && (texto2[j] != '\0'))
+        {
+            putchar(texto2[j]);
+            j++;
+        }
+        fflush(stdin);
+
+        printf(" ");
+
+        i++;
+        j++;
+    }
+
+    printf("\n");
 }
