e8d1e9238072f9f246aa44bdab08a8610cb1bef2
sproods <106177602+sproods@users.noreply.github.com>
2022-11-11 14:07:02 -0500

    Contador de las veces que se repite un caracter en un archivo de texto.

---
+++ Archivos de Datos/Problemas Resueltos/problema2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/* Cuenta caracteres.
+ * El programa, al recibir como dato un archivo de texto y un caracter,
+ * cuenta el número de veces que se encuentra el caracter en el archivo. */
+
+int cuenta(char);           /* Prototipo de función. */
+
+void main(void)
+{
+    int res;
+    char car;
+
+    printf("\nDigite el caracter que se va a buscar en el archivo: ");
+    car = getchar();
+    res = cuenta(car);
+
+    if (res != -1)
+        printf("\n\nEl caracter %c se encuentra en el archivo %d veces.\n", car, res);
+    else
+        printf("\nNo se puede abrir el archivo.\n");
+}
+
+int cuenta(char car)
+/* Esta función determina cuántas veces se encuentra el caracter en el archivo. */
+{
+    int res, con = 0;
+    char p;
+    FILE *ar;
+
+    if ((ar = fopen("arc.txt", "r")) != NULL)       /* Se abre el archivo para lectura. */
+    {
+        while (!feof(ar))       /* Se trabaja con el archivo mientras no se llegue al fin de éste. */
+        {
+            p = getc(ar);
+            if (p == car)       /* Se realiza la comparación de los caracteres. */
+            con++;
+        }
+        fclose(ar);
+        res = con;
+    }
+    else
+        res = -1;
+
+        return (res);
+}
