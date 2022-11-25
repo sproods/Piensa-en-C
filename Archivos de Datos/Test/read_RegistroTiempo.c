afbef82e4d77a74d285a2bc6e7d529b58db60955
sproods <106177602+sproods@users.noreply.github.com>
2022-11-25 11:29:29 -0500

    lectura de los datos del archivo de texto habitos.txt... no funciona en este intento.

--- Archivos de Datos/Test/read_RegistroTiempo.c
+++ Archivos de Datos/Test/read_RegistroTiempo.c
@@ -4,13 +4,12 @@
 #include <string.h>
 
 #define N 30
-#define M 50
 
 void main(void)
 {
     int i, j, n_Time, n_Act;
-    char char_Tiempo[15], activity[N][30];
-    float tiempo[M];
+    char char_Tiempo[15], activity[N][N], actividad[N];
+    float tiempo[N][N];
     FILE *arxiv;
 
     arxiv = fopen("habitos.txt", "r");
@@ -19,43 +18,43 @@ void main(void)
     {
         putchar('\n');
 
-        while (!feof(arxiv))
+        fgets(char_Tiempo, 15, arxiv);              // Obtenemos el tiempo que hemos puesto como control.
+
+        puts(char_Tiempo);                          // Escribimos en pantalla la cadena de caracteres que obtenemos.
+
+        fscanf(arxiv, "%d", &n_Time);               // Obtenemos el número del tiempo en que se van a registrar las actividades.
+        fflush(stdin);
+
+        printf("%d", n_Time);                       // Imprimimos en pantalla el valor de n_Time.
+
+        fscanf(arxiv, "%d", &n_Act);                // Obtenemos el número de actividades.
+        fflush(stdin);
+
+        printf("\n%d", n_Act);                      // Imprimimos en pantalla el valor de n_Act.
+
+        for (i = 0; i < n_Act; i++)
         {
-            // Obtenemos el tiempo que hemos puesto como control.
-            fgets(char_Tiempo, 15, arxiv);
-
-            // Escribimos en pantalla la cadena de caracteres que obtenemos.
-            puts(char_Tiempo);
-
-            // Obtenemos el número del tiempo en que se van a registrar las actividades.
-            fscanf(arxiv, "%d", &n_Time);
+            fgets(actividad, N, arxiv);          // Obtenemos y almacenamos el nombre de cada una de las actividades.
+            strcpy(activity[i], actividad);
             fflush(stdin);
+            // puts(activity[i]);                      // Imprimimos en pantalla el nombre de las actividades.
 
-            // Imprimimos en pantalla el valor de n_Time.
-            printf("%d", n_Time);
-
-            // Obtenemos el número de actividades.
-            fscanf(arxiv, "%d", &n_Act);
-            fflush(stdin);
-
-            // Imprimimos en pantalla el valor de n_Act.
-            printf("\n%d", n_Act);
-
-            for (i = 0; i < n_Act; i++)
+            for (j = 0; j < n_Time; j++)
             {
-                putchar('\n');
-                fgets(activity[i], 30, arxiv);          // Obtenemos y almacenamos el nombre de cada una de las actividades.
-                puts(activity[i]);                      // Imprimimos en pantalla el nombre de las actividades.
-
-                for (j = 0; j < n_Time; j++)
-                {
-                    fscanf(arxiv, "%f", &tiempo[j]);     // Obtenemos el valor de cada uno de los tiempos escritos en el archivo.
-                    fflush(stdin);
-                    printf("\n%.2f", tiempo[j]);          // Escribimos en pantalla el valor de cada uno de los tiempos.
-                }
+                fscanf(arxiv, "%f", &tiempo[i][j]);    // Obtenemos el valor de cada uno de los tiempos escritos en el archivo.
+                fflush(stdin);
+                // printf("\n%.2f", tiempo[i][j]);        // Escribimos en pantalla el valor de cada uno de los tiempos.
             }
-
         }
+
+        for (i = 0; i < n_Act; i++)
+        {
+            printf("\n%s", activity[i]);
+
+            for (j = 0; j < n_Time; j++)
+                printf("\n%.2f", tiempo[i][j]);
+        }
+
         fclose(arxiv);
     }
     else
