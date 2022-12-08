33ef91212a0a4b769f797c8604c6ebbeca407def
sproods <106177602+sproods@users.noreply.github.com>
2022-12-08 13:20:15 -0500

    resolución del problema cinco. un archivo con las líneas de texto invertidas con respecto a otro archivo.

--- Archivos de Datos/Problemas Suplementarios/problema05.c
+++ Archivos de Datos/Problemas Suplementarios/problema05.c
@@ -1,6 +1,9 @@
 // programa que realiza la inversión en la posición de cada línea de de texto en un archivo de acceso directo.
 
 #include <stdio.h>
+#include <string.h>
+
+#define MAX 50
 
 void invierte(FILE *, FILE *);			// prototipo de función
 
@@ -9,7 +12,7 @@ void main(void)
 	FILE *arxiv1, *arxiv2;
 	
 	arxiv1 = fopen("arc4.txt", "r");
-	arxiv2 = fopen("arc5.txt", "a+");
+	arxiv2 = fopen("arc5.txt", "w");
 	
 	if ((arxiv1 != NULL) && (arxiv2 != NULL))
 	{
@@ -23,13 +26,45 @@ void main(void)
 
 void invierte(FILE *arc1, FILE *arc2)
 {
-	char cadena[250];
+	int lines = 0, i;
+	char dance[250], strings[MAX][250];
 	
+	// determinamos el número de líneas de texto que hay en el archivo.
 	while (!feof(arc1))
 	{
-		fgets(cadena, 250, arc1);
-		//rewind(arc2);
-		//fputs("\n", arc2);
-		fputs(cadena, arc2);
+		fgets(dance, 250, arc1);
+		lines++;
+	}
+	printf("La cantidad de líneas de texto es: %d", lines);
+	
+	rewind(arc1);
+	
+	// almacenamos cada cadena de caracteres en un arreglo de tipo cadena de caracteres
+	while (!feof(arc1))
+	{
+		for (i = lines - 1; i >= 0; i--)
+		{
+			fgets(dance, 250, arc1);
+			strcpy(strings[i], dance);
+		}
+	}
+	
+	// segundo método de almacenar
+	/* for (i = lines - 1; i >= 0; i--)
+	{
+		fgets(dance, 250, arc1);
+		strcpy(strings[i], dance);
+	} */
+	
+	// escribimos las cadenas, invertidas, en el segundo archivo
+	for (i = 0; i < lines; i++)
+	{
+		if (i == 0)
+		{
+			fputs(strings[i], arc2);
+			fputs("\n", arc2);
+		}
+		else
+			fprintf(arc2, "%s", strings[i]);
 	}
 }
