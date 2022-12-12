5122ab7608ec3f4dad079ffadb253527d434e63c
sproods <106177602+sproods@users.noreply.github.com>
2022-12-11 23:31:52 -0500

    mejora en la resolución del problema siete.

--- Archivos de Datos/Problemas Suplementarios/problema07.c
+++ Archivos de Datos/Problemas Suplementarios/problema07.c
@@ -28,6 +28,27 @@ void main(void)
 
 void ordena(FILE *arc)
 {
-    int i;
+    int i, tam, may_mat = 0, index = 0;
+    Datos datos_alumno;
 
+    tam = sizeof(Datos);        // definimos el tamaño de cada bloque de estructura
+
+    fseek(arc, 0, 0);           // nos posicionamos al inicio del archivo
+    fread(&datos_alumno, sizeof(Datos), 1, arc);
+
+    while (!feof(arc))
+    {
+        i = ftell(arc) / tam;
+
+        if (datos_alumno.mat > may_mat)
+        {
+            may_mat = datos_alumno.mat;
+            index = i;
+        }
+
+        fseek(arc, i * sizeof(Datos), 0);
+        fread(&datos_alumno, sizeof(Datos), 1, arc);
+    }
+
+    printf("\nLa matrícula mayor es %d ubicada en el índice %d\n", may_mat, index);
 }
